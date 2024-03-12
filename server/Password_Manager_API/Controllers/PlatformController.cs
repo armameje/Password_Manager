60d404d221069651bf7072701f4caaf1eb21ca05
armameje <armamentojenisi@gmail.com>
2024-03-13 04:58:14 +0800

    Add initial API setup; add controllers

---
+++ server/Password_Manager_API/Controllers/PlatformController.cs
@@ -0,0 +1,42 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Password_Manager_API.Controllers
+{
+    [ApiController]
+    [ApiVersion(1.0)]
+    [Route("[controller]")]
+    public class PlatformController : ControllerBase
+    {
+
+        [HttpGet]
+        [MapToApiVersion(1.0)]
+        [Route("{user}")]
+        public async Task<IActionResult> AllUserAccounts()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> AllPlatforms()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> AddPlatform()
+        {
+            return Ok();
+
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> AddAccount()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ server/Password_Manager_API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Password_Manager_API.Controllers
+{
+    [ApiController]
+    [ApiVersion(1.0)]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [MapToApiVersion(1.0)]
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+
+        [MapToApiVersion(1.0)]
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+    }
+}
